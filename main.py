199238c9b25a5204619e820b03428abb7d56cb71
sueyoshiryosuke <sueyoshi.ryosuke@it-neec.jp>
2020-11-30 02:57:41 +0000

    ぐう、ちょきのときは完成。残りはぱあのとき。

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 # Faskのインスタンスを作成
 app = Flask(__name__)
 
-# ルーティングの指定
+# ルーティングの指定（最初のページ）
 @app.route('/')
 def index():
-    return "じゃんけんアプリ"
+  #index.hthlのテンプレートを設定する
+  return render_template("index.html")
 
+# ルーティングの指定（ぐうのとき）
 @app.route('/guu')
 def guu():
-    return "ぐー"
+  #ロボットの出す手を決める
+  #乱数を使う
+  robot = random.randint(1,3)
 
-# デバッグモードでサーバを起動させる
-#app.run(debug=True)
-app.run(host='0.0.0.0', port=3000)
+  return render_template("guu.html", robot=robot)
+
+# ルーティングの指定（ちょきのとき）
+@app.route('/choki')
+def choki():
+  #ロボットの出す手を決める
+  #乱数を使う
+  robot = random.randint(1,3)
+
+  return render_template("choki.html", robot=robot)
+
+# ルーティングの指定（ぱあのとき）。未完
+
+
+app.run(host='0.0.0.0')
